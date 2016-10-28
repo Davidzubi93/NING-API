27f272fd3ee00f602b9ca91cf385802a1995fc16
davidzubi <davidzubi93@gmail.com>
2016-10-28 10:45:51 +0000

    irudi berri bat gehitzeko erabiltzen den fitxategia

---
+++ addphoto.php
@@ -0,0 +1,14 @@
+<?php
+require_once('ning-api-php/NingApi.php');
+
+$parts = array(
+    "title" => "Titulo Foto",
+    "description" => "Descripcion Foto",
+    "file" => "@/Users/devin/Pictures/"
+    );
+    
+// Crear una nueva foto
+    $resultado = NingApi::instance()->photo->create($parts);
+    
+    print_r($resultado);
+?>
